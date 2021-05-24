c372aa3da967b22fc92828b3ff19b4fd5a1c04bd
Yangson Hung <2545594421@qq.com>
2021-05-24 08:22:52 +0800

    ‘âadd test

---
+++ SVG/react-svg-demo/demo/src/App.js
@@ -0,0 +1,176 @@
+import React, { useState, useRef, useEffect } from 'react';
+// require("./ripple-min");
+
+const circleInnerProps = {
+    id: 'circle1',
+    cx: 50,
+    cy: 50,
+    r: 50 - 2,
+    stroke: '#eee',
+    strokeWidth: 2,
+    fill: 'none',
+};
+
+const circleOuterInitProps = {
+    id: 'circle2',
+    cx: 50,
+    cy: 50,
+    r: 50 - 5,
+    stroke: 'blue',
+    strokeWidth: 5,
+    fill: 'none',
+    strokeDasharray: `0 ${Math.ceil(2 * Math.PI * (50 - 5))}`,
+    style: {
+        transform: 'rotate(-90deg)',
+        transformOrigin: 'center',
+    },
+};
+
+const textProps = {
+    x: '50%',
+    y: '50%',
+    fill: 'blue',
+    style: {
+        fontSize: '22px',
+        fontWeight: '700',
+    },
+    alignmentBaseline: 'middle',
+    textAnchor: 'middle',
+};
+// 外圆周长
+const perimeter = 2 * Math.PI * circleOuterInitProps.r;
+
+export const Demo1 = ({ initValue = 0 }) => {
+    const [rangeValue, setRangeValue] = useState(initValue);
+    const [circleOuterProps, setCircleOuterProps] = useState(circleOuterInitProps);
+    /**
+     * @description 改变圆strokeDasharray
+     * @param {*} e
+     */
+    const onRangeChange = e => {
+        const {
+            target: { value },
+        } = e;
+        setRangeValue(value);
+        const percent = value / 100;
+        const d1 = percent * perimeter;
+        const d2 = (1 - percent) * perimeter;
+        const strokeDasharray = `${d1} ${d2}`;
+
+        setCircleOuterProps({
+            ...circleOuterProps,
+            strokeDasharray,
+        });
+    };
+
+    return (
+        <div>
+            <div>demo1</div>
+            <svg width="100" height="100">
+                <circle {...circleInnerProps} />
+                <circle {...circleOuterProps} />
+                <text {...textProps}>{rangeValue}</text>
+            </svg>
+            <input onChange={onRangeChange} value={rangeValue} type="range" min="0" max="100" step="0.01" />
+        </div>
+    );
+};
+
+export const Demo2 = () => {
+    const ref = useRef(null);
+
+    useEffect(() => {
+        console.log('ref.current :>> ', ref.current);
+    }, []);
+
+    return (
+        <div>
+            demo2
+            <br />
+            <img
+                ref={ref}
+                id="img"
+                width="200"
+                src="https://mdn.mozillademos.org/files/6457/mdn_logo_only_color.png"
+                alt=""
+            />
+        </div>
+    );
+};
+
+export const Demo3 = () => {
+    const [cx1, setCx1] = useState(120);
+    const [cx2, setCx2] = useState(400);
+    const [r, setR] = useState(90);
+    const [turn, setTurn] = useState(true);
+
+    const onClick = () => {
+        const move = document.getElementById('move');
+        // const scale = document.getElementById('scale');
+        if (turn) {
+
+            setCx1(120);
+            setCx2(400);
+            // setR(90);
+            move.beginElement();
+            // scale.beginElement();
+            setTurn(false);
+        } else {
+
+            setCx1(400);
+            setCx2(120);
+            // setR(50);
+            move.beginElement();
+            // scale.beginElement();
+            setTurn(true);
+        }
+    };
+
+    return (
+        <div>
+            <div>demo3</div>
+            <div>
+                <button onClick={onClick}>按钮</button>
+            </div>
+            <svg width="100%" height="400">
+                <circle cx="120" cy="100" r="50" fill="none" stroke="red">
+                    <animate
+                        id="move"
+                        attributeType="XML"
+                        attributeName="cx"
+                        form={cx1}
+                        to={cx2}
+                        begin="btn.click"
+                        dur="1s"
+                        fill="freeze"
+                    />
+                    <animate
+                        id="scale"
+                        attributeType="XML"
+                        attributeName="r"
+                        to={r}
+                        // begin="btn.click"
+                        dur="1s"
+                        fill="freeze"
+                    />
+                    {/* <animateTransform
+                        id="scale"
+                        attributeType="XML"
+                        attributeName="transform"
+                        type="scale"
+                        from="1"
+                        to="1.7"
+                        dur="3s"
+                        // begin="0"
+                        // fill="freeze"
+                        // additive="sum"
+                        // repeatCount="indefinite"
+                    /> */}
+                </circle>
+                {/* <text id="btn" x="10" y="50" fill="blue">
+                        点击
+                </text> */}
+            </svg>
+        </div>
+    );
+};
---
+++ SVG/react-svg-demo/demo/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import { Demo1, Demo2, Demo3 } from './App';
+
+ReactDOM.render(
+    <React.StrictMode>
+        <div>
+            <Demo1 />
+            <Demo2 />
+            <Demo3 />
+        </div>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
